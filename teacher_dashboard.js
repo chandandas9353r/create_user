f2b1901b7826eae19492048a8850009e9fc73c30
chandandas9353r <chandandas9353r@gmail.com>
2023-05-10 01:49:51 +0530

    stage2

--- teacher_dashboard.js
+++ teacher_dashboard.js
@@ -21,46 +21,45 @@ onAuthStateChanged(auth, (user) => {
     else loggedIn()
 })
 
+function createSection(element, value, div1, div2) {
+    let item = document.createElement(element)
+    if (value != '') item.innerHTML = value
+    div1.append(item)
+    if (div2 != null) div2.append(div1)
+}
+
 function loggedIn() {
     let createClassCard = document.getElementById('createClassCard'),
         content = document.getElementById('content'),
         nolecture = document.getElementById('nolecture'),
         lecturesList = document.getElementById('lecturesList'),
         date = new Date(),
-        currentDate = (date.getDate() < 10) ? `0${date.getDate()}` : date.getDate()
+        currentDate = (date.getDate() < 10) ? `0${date.getDate()}` : date.getDate(),
+        uid = auth.currentUser.uid
     currentDate = (date.getMonth() + 1 < 10) ? `${currentDate}:0${date.getMonth() + 1}:${date.getFullYear()}` : `${currentDate}:${date.getMonth() + 1}:${date.getFullYear()}`
-    
-    onValue(ref(database,`classes/`), (snapshot) => {
-        if(snapshot.val() == null) nolecture.innerHTML = 'No lectures has been set'
-        else{
-            nolecture.style.display = 'none'
-            lecturesList.style.display = 'flex'
-            let section = document.createElement('div')
-            section.setAttribute('class','cards')
-            let item = document.createElement('p')
-            item.innerHTML = 'COMPILER'
-            section.append(item)
-            item = document.createElement('div')
-            let desc = document.createElement('span')
-            desc.innerHTML = 'Class starts at 11:00 AM'
-            item.append(desc)
-            section.append(item)
-            desc = document.createElement('br')
-            item.append(desc)
-            section.append(item)
-            desc = document.createElement('span')
-            desc.innerHTML = 'Dept : BCA'
-            item.append(desc)
-            section.append(item)
-            desc = document.createElement('br')
-            item.append(desc)
-            section.append(item)
-            desc = document.createElement('span')
-            desc.innerHTML = 'Semester : 6th'
-            item.append(desc)
-            section.append(item)
-            lecturesList.append(section)
-        }
+
+    onValue(ref(database), (snapshot) => {
+        let dept = snapshot.child(`users/teacher/${uid}/Department`).val()
+        onValue(ref(database, `classes/teachers/${dept}/${uid}/`), (snapshot) => {
+            if (snapshot.val() == null) nolecture.innerHTML = 'No lectures has been set.... '
+            else {
+                nolecture.style.display = 'none';
+                lecturesList.style.display = 'flex';
+                (snapshot.forEach(element => {
+                    let sub = element.key
+                    let section = document.createElement('div'),
+                        item = document.createElement('div')
+                    section.setAttribute('class', 'cards')
+                    createSection('p', element.key, section, null)
+                    createSection('span', `Class starts at ${snapshot.child(element.key).val().Time}`, item, section)
+                    createSection('br', '', item, section)
+                    createSection('span', `Dept : ${dept}`, item, section)
+                    createSection('br', '', item, section)
+                    createSection('span', `Semester : ${snapshot.child(element.key).val().Semester}`, item, section)
+                    lecturesList.append(section)
+                }))
+            }
+        })
     })
 
     document.querySelector('.container > .container1').addEventListener('click', openCreateCard)
@@ -69,25 +68,27 @@ function loggedIn() {
         content.style.display = 'none'
         createClassCard.style.display = 'block'
         let create = document.getElementById('create'),
-            cancel = document.getElementById('cancel'),
-            uid = auth.currentUser.uid
+            cancel = document.getElementById('cancel')
         create.addEventListener('click', function () {
             onValue(ref(database), (snapshot) => {
                 let date = new Date(),
-                    currentDate = (date.getDate() < 10) ? `0${date.getDate()}` : date.getDate(),
                     dept = snapshot.child(`users/teacher/${uid}/Department`).val(),
                     teacherName = snapshot.child(`users/teacher/${uid}/Name`).val(),
+                    currentDate = (date.getDate() < 10) ? `0${date.getDate()}` : date.getDate(),
                     sem = document.getElementById('sem').value,
                     sub = document.getElementById('sub').value,
-                    room = `${document.getElementById('room').value}`,
+                    room = `R-${document.getElementById('room').value}`,
                     time = document.getElementById('time').value
                 currentDate = (date.getMonth() + 1 < 10) ? `${currentDate}:0${date.getMonth() + 1}:${date.getFullYear()}` : `${currentDate}:${date.getMonth() + 1}:${date.getFullYear()}`
-                console.log(teacherName,currentDate, dept, sem, sub, room, time)
-                set(ref(database,`classes/${currentDate}/${dept}/${sem}`), {
-                    'Subject': sub,
+                set(ref(database, `classes/teachers/${dept}/${uid}/${sub}/`), {
+                    'Semester': sem,
                     'Room': room,
-                    'Teacher': teacherName,
                     'Time': time
+                })
+                set(ref(database,`classes/${currentDate}/${dept}/${sem}/${sub}/`), {
+                    'Teacher': teacherName,
+                    'Time': time,
+                    'Room': room
                 })
             })
             window.setTimeout(() => {
